d72d1322c336d16d2208258e01636c409ce8282d
Sten Vercammen <ulf1103@me.com>
2018-05-15 10:04:39 +0200

    fix: removed forced INFO logging from tasKIT

--- master/tasKIT/tasKIT.py
+++ master/tasKIT/tasKIT.py
@@ -16,9 +16,6 @@ LOG_FORMAT = ('%(levelname) -10s %(asctime)s %(name) -30s %(funcName) '
               '-35s %(lineno) -5d: %(message)s')
 LOGGER = logging.getLogger(__name__)
 
-logging.basicConfig(level=logging.INFO, format=LOG_FORMAT)
-
-
 class FunctionRunner:
     """
     Stores external functions, so that they only need to be imported once.
